e96bca1b30d391de993234b063bae8da6e369e15
ielgnaw <wuji0223@gmail.com>
2014-10-04 14:22:42 +0800

    BOM & .csshintrc

---
+++ hint/no-bom.js
@@ -0,0 +1,20 @@
+/**
+ * @file no-bom 检测
+ * @author ielgnaw(wuji0223@gmail.com)
+ */
+
+/**
+ * 模块的输出接口
+ *
+ * @param {string} bufContent 当前检测文件 buffer 内容
+ * @param {Array.<Object>} invalidList 不合法文件集合
+ */
+module.exports = function (bufContent, invalidList) {
+    // var hasBOM = fileContent[0] === 0xEF
+        // && fileContent[1] === 0xBB
+        // && fileContent[2] === 0xBF;
+
+    // console.log(hasBOM);
+
+    // console.log(bufContent);
+};
