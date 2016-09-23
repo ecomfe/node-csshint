055acb4373acfa76d5a85c9c5266ea395f4a5472
ielgnaw <wuji0223@gmail.com>
2016-09-24 02:06:30 +0800

    配置文件统一为 .csshintrc

--- src/config.js
+++ src/config.js
@@ -4,19 +4,17 @@
  */
 
 import Manis from 'manis';
-import {join, basename} from 'path';
-import stripJSONComments from 'strip-json-comments';
+import {join} from 'path';
 import yaml from 'js-yaml';
 
 'use strict';
 
 let STORAGE = null;
 
-const JSON_YAML_REG = /.+\.(json|yml)$/i;
+// const JSON_YAML_REG = /.+\.(json|yml)$/i;
 
 /**
  * 获取 merge 后的配置文件
- * 用户自定义的配置文件优先级 .csshintrc > config.yml > config.json
  *
  * @param {string} filePath 待检查的文件路径，根据这个路径去寻找用户自定义的配置文件，然后和默认的配置文件 merge
  * @param {boolean} refresh 是否强制刷新内存中已经存在的配置
@@ -30,34 +28,37 @@ export function loadConfig(filePath, refresh) {
 
     let manis = new Manis({
         files: [
-            '.csshintrc',
-            'config.yml',
-            'config.json'
+            '.csshintrc'
         ],
-        loader(content, filePath) {
+        loader(content) {
             if (!content) {
                 return '';
             }
-
-            if (basename(filePath) === '.csshintrc') {
-                return JSON.parse(stripJSONComments(content));
+            let ret;
+            try {
+                ret = yaml.load(content);
             }
-
-            let match = filePath.match(JSON_YAML_REG);
-
-            if (match) {
-                let suffix = match[1];
-                if (suffix === 'json') {
-                    return JSON.parse(stripJSONComments(content));
-                }
-                else if (suffix === 'yml') {
-                    return yaml.load(content);
-                }
-            }
+            catch (e) {}
+            // if (basename(filePath) === '.csshintrc') {
+            //     return JSON.parse(stripJSONComments(content));
+            // }
+            //
+            // let match = filePath.match(JSON_YAML_REG);
+            //
+            // if (match) {
+            //     let suffix = match[1];
+            //     if (suffix === 'json') {
+            //         return JSON.parse(stripJSONComments(content));
+            //     }
+            //     else if (suffix === 'yml') {
+            //         return yaml.load(content);
+            //     }
+            // }
+            return ret;
         }
     });
 
-    manis.setDefault(join(__dirname, './config.yml'));
+    manis.setDefault(join(__dirname, './csshint.yml'));
 
     STORAGE = manis.from(filePath);
 
